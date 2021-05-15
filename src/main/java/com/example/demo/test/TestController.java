10f012ebb7fea028f8099464ddf60bb61c93ecd6
dongwantak <dongwantak@gmail.com>
2021-05-15 21:44:11 +0900

    이미지리스트, 액박오류수정

--- src/main/java/com/example/demo/test/TestController.java
+++ src/main/java/com/example/demo/test/TestController.java
@@ -1,29 +1,87 @@
 package com.example.demo.test;
 
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousFileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.Future;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.model.Follow;
 import com.example.demo.model.Image;
 import com.example.demo.model.Likes;
 import com.example.demo.model.User;
+import com.example.demo.repository.ImageRepository;
 import com.example.demo.repository.UserRepository;
-
-
-
+import com.example.demo.service.MyUserDetail;
+import com.example.demo.model.Tag;
+import com.example.demo.util.Utils;
 
 @Controller
 public class TestController {
+
 	@Autowired
 	private UserRepository mUserRepository;
+
+	@Autowired
+	private ImageRepository mImageRepository;
+
+	@Value("${file.path}")
+	private String fileRealPath;
+
+	// 이미지 동기처리 하기!!
+	@PostMapping("/test/image/uploadProc")
+	public String testImageUploadProc(@AuthenticationPrincipal MyUserDetail userDetail,
+			@RequestParam("file") MultipartFile file, @RequestParam("caption") String caption,
+			@RequestParam("location") String location, @RequestParam("tags") String tags) throws IOException{
+
+		// 이미지 업로드 수행
+		UUID uuid = UUID.randomUUID();
+		String uuidFilename = uuid + "_" + file.getOriginalFilename();
+
+		Path filePath = Paths.get(fileRealPath + uuidFilename);
+		if (!Files.exists(filePath)) {
+			Files.createFile(filePath);
+		}
+
+		AsynchronousFileChannel fileChannel = AsynchronousFileChannel.open(filePath, StandardOpenOption.WRITE);
+
+		ByteBuffer buffer = ByteBuffer.allocate((int) file.getSize());
+
+		buffer.put(file.getBytes());
+		buffer.flip();
+
+		Future<Integer> operation = fileChannel.write(buffer, 0);
+		buffer.clear();
+
+		while (!operation.isDone());
+
+		System.out.println("Write done");
+
+		return "redirect:/";
+	}
 
 	@GetMapping("/test/user/{id}")
 	public @ResponseBody User testUser(@PathVariable int id) {
@@ -31,6 +89,7 @@ public class TestController {
 		User user = oUser.get();
 		return user;
 	}
+	
 	@GetMapping("/test/home")
 	public String testHome() {
 		return "home";
@@ -215,10 +274,10 @@ public class TestController {
 	public String testImageUpload() {
 		return "image/image_upload";
 	}
-
+	
 	@GetMapping("/test/explore")
 	public String testExplore() {
 		return "image/explore";
 	}
+	
 }
-
